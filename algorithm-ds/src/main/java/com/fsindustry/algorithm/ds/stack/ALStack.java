82538205fdbab0f9143d96526f1bd89717830ad7
fuzhengxin <fuzhengxin@baidu.com>
2018-06-12 16:37:19 +0800

    栈的应用： 1）数制转换，输出结果翻转； 2）括号对匹配；

---
+++ algorithm-ds/src/main/java/com/fsindustry/algorithm/ds/stack/ALStack.java
@@ -0,0 +1,37 @@
+package com.fsindustry.algorithm.ds.stack;
+
+import java.util.ArrayList;
+
+/**
+ * <h1>基于ArrayList的栈的实现</h1>
+ *
+ * @author fuzhengxin
+ */
+public class ALStack<T> {
+
+    private ArrayList<T> arr;
+
+    public ALStack() {
+        this.arr = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return arr.isEmpty();
+    }
+
+    public T peek() {
+        return arr.get(arr.size() - 1);
+    }
+
+    public T pop() {
+        return arr.remove(arr.size() - 1);
+    }
+
+    public void push(T item) {
+        arr.add(item);
+    }
+
+    public int size() {
+        return arr.size();
+    }
+}
